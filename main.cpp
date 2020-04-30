26fb8f3865fed90a81a01fd2d70aa046704dcb53
atoka <makrusan@gmail.com>
2020-04-30 13:44:51 +0300

    Make padle movable

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 #include <vector>
 #include <SFML/Graphics.hpp>
+#include <SFML/System/Time.hpp>
 
-const std::vector<sf::Color> palette {
-    sf::Color(0x264653ff),
-    sf::Color(0x2a9d8fff),
-    sf::Color(0xe9c46aff),
-    sf::Color(0xf4a261ff),
-    sf::Color(0xe76f51ff)
+const std::vector<sf::Color> palette{
+        sf::Color(0x264653ff),
+        sf::Color(0x2a9d8fff),
+        sf::Color(0xe9c46aff),
+        sf::Color(0xf4a261ff),
+        sf::Color(0xe76f51ff)
 };
 
 class Padle {
@@ -23,29 +24,46 @@ public:
         app.draw(r);
     }
 
+    void moveLeft(const sf::Time &elapsed) { x -= speed * elapsed.asSeconds(); }
+
+    void moveRight(const sf::Time &elapsed) { x += speed * elapsed.asSeconds(); }
+
     Padle(float x, float y, float width, float height, float speed)
             : x(x), y(y), width(width), height(height), speed(speed) {}
 };
 
 int main() {
-    sf::RenderWindow app(sf::VideoMode(640, 480), "Arkanoid", sf::Style::Fullscreen);
+    sf::RenderWindow app(sf::VideoMode(640, 480), "Arkanoid", sf::Style::Default);
 
     float pw = 160;
     float ph = 16;
-    Padle padle((app.getSize().x - pw) / 2, app.getSize().y - ph - 20, pw, ph, 500);
+    Padle padle((app.getSize().x - pw) / 2, app.getSize().y - ph - 20, pw, ph, 1000);
 
-    while(app.isOpen()) {
-       sf::Event event;
-       while(app.pollEvent(event)) {
-           if (event.type == sf::Event::Closed) {
-               app.close();
-           }
-       }
-       app.clear(palette[0]);
+    sf::Clock clock;
 
-       padle.draw(app);
+    while (app.isOpen()) {
+        auto elapsed = clock.restart();
 
-       app.display();
+        sf::Event event;
+        while (app.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                app.close();
+            }
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Left)) {
+            padle.moveLeft(elapsed);
+        }
+
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Right)) {
+            padle.moveRight(elapsed);
+        }
+
+        app.clear(palette[0]);
+
+        padle.draw(app);
+
+        app.display();
     }
 
     return 0;
